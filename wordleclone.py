e3a5c87fa5b438abf28e24ae8f6f9a005ce37fea
Abhishek Singh <273802218+abhisheksingh-chi@users.noreply.github.com>
2026-07-09 16:09:39 +0530

    Enhance word guessing logic and display
    Added input normalization and improved output formatting.

--- wordleclone.py
+++ wordleclone.py
@@ -2,11 +2,11 @@ import random
 words =random.choice(["words","ready"])
 atem = 0
 correct = ['_', '_', '_', '_', '_']
-
+formatted = " "
 print("- - - - -\n"*6,end="")
 while atem < 6:
     atem += 1
-    guess = input("Enter 5 letter word: ")
+    guess = input("Enter 5 letter word: ").lower()
     if len(guess)<5:
         print("Please enter a 5 letter word")
 #Exception handling for IndexError when len(guess)<len(words)      
@@ -15,8 +15,11 @@ while atem < 6:
             correct[index] = guess[index]
         else:
             correct[index]= "-"
-    print(correct)
+    formatted = '  '.join(correct)
+    print(formatted.upper())
     if guess == words:
+        print(f"Congrats! you guessed the right word which was '{words}'")
         break
                 
+   
    
